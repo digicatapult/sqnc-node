a7c098e1d567bf9191e23977de4f61c60e69e5d8
Callum Hyland <callum.hyland@digicatapult.org.uk>
2021-07-09 11:03:25 +0100

    Add rustfmt.toml + update CI workflow

--- pallets/simple-nft/src/benchmarking.rs
+++ pallets/simple-nft/src/benchmarking.rs
@@ -12,45 +12,40 @@ use crate::Module as SimpleNFT;
 const SEED: u32 = 0;
 
 fn add_nfts<T: Config>(r: u32) -> Result<(), &'static str> {
-  let owner: T::AccountId = account("owner", 0, SEED);
-  // let _ = T::Currency::make_free_balance_be(&owner, BalanceOf::<T>::max_value());
+    let owner: T::AccountId = account("owner", 0, SEED);
+    // let _ = T::Currency::make_free_balance_be(&owner, BalanceOf::<T>::max_value());
 
-  let outputs: Vec<_> = (0..r)
-    .map(|_| (owner.clone(), T::TokenMetadata::default()))
-    .collect();
-  SimpleNFT::<T>::run_process(RawOrigin::Signed(owner.clone()).into(), Vec::new(), outputs)?;
+    let outputs: Vec<_> = (0..r).map(|_| (owner.clone(), T::TokenMetadata::default())).collect();
+    SimpleNFT::<T>::run_process(RawOrigin::Signed(owner.clone()).into(), Vec::new(), outputs)?;
 
-  let expected_last_token = nth_token_id::<T>(r)?;
+    let expected_last_token = nth_token_id::<T>(r)?;
 
-  assert_eq!(LastToken::<T>::get(), expected_last_token);
-  assert_eq!(
-    TokensById::<T>::iter_values().collect::<Vec<_>>().len(),
-    r as usize
-  );
-  Ok(())
+    assert_eq!(LastToken::<T>::get(), expected_last_token);
+    assert_eq!(TokensById::<T>::iter_values().collect::<Vec<_>>().len(), r as usize);
+    Ok(())
 }
 
 fn mk_inputs<T: Config>(i: u32) -> Result<Vec<T::TokenId>, &'static str> {
-  let inputs = (0..i).fold(Vec::<T::TokenId>::new(), |mut acc, _| {
-    acc.push(*acc.last().unwrap_or(&T::TokenId::default()) + One::one());
-    acc
-  });
+    let inputs = (0..i).fold(Vec::<T::TokenId>::new(), |mut acc, _| {
+        acc.push(*acc.last().unwrap_or(&T::TokenId::default()) + One::one());
+        acc
+    });
 
-  Ok(inputs)
+    Ok(inputs)
 }
 
 fn mk_outputs<T: Config>(o: u32) -> Result<Vec<(T::AccountId, T::TokenMetadata)>, &'static str> {
-  let owner: T::AccountId = account("owner", 0, SEED);
-  let outputs = (0..o)
-    .map(|_| (owner.clone(), T::TokenMetadata::default()))
-    .collect::<Vec<_>>();
+    let owner: T::AccountId = account("owner", 0, SEED);
+    let outputs = (0..o)
+        .map(|_| (owner.clone(), T::TokenMetadata::default()))
+        .collect::<Vec<_>>();
 
-  Ok(outputs)
+    Ok(outputs)
 }
 
 fn nth_token_id<T: Config>(iteration: u32) -> Result<T::TokenId, &'static str> {
-  let token_id = (0..iteration).fold(T::TokenId::default(), |acc, _| acc + One::one());
-  Ok(token_id)
+    let token_id = (0..iteration).fold(T::TokenId::default(), |acc, _| acc + One::one());
+    Ok(token_id)
 }
 
 benchmarks! {
--- pallets/simple-nft/src/tests.rs
+++ pallets/simple-nft/src/tests.rs
@@ -1,286 +1,347 @@
 // Tests to be written here
 
-use crate::{Error, Token, mock::*};
-use frame_support::{assert_ok, assert_err};
+use crate::{mock::*, Error, Token};
+use frame_support::{assert_err, assert_ok};
 
 #[test]
 fn it_works_for_creating_simple_token() {
-	new_test_ext().execute_with(|| {
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)] ));
-		// last token should be 1
-		assert_eq!(SimpleNFT::last_token(), 1);
-		// get the token
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: None
-		});
-	});
+    new_test_ext().execute_with(|| {
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)]));
+        // last token should be 1
+        assert_eq!(SimpleNFT::last_token(), 1);
+        // get the token
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: None
+            }
+        );
+    });
 }
 
 #[test]
 fn it_works_for_creating_many_token() {
-	new_test_ext().execute_with(|| {
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43), (1, 44)] ));
-		// last token should be 3
-		assert_eq!(SimpleNFT::last_token(), 3);
-		// get the token
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: None
-		});
-		let token = SimpleNFT::tokens_by_id(2);
-		assert_eq!(token, Token {
-			id: 2,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 43,
-			parents: Vec::new(),
-			children: None
-		});
-		let token = SimpleNFT::tokens_by_id(3);
-		assert_eq!(token, Token {
-			id: 3,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 44,
-			parents: Vec::new(),
-			children: None
-		});
-	});
+    new_test_ext().execute_with(|| {
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(
+            Origin::signed(1),
+            Vec::new(),
+            vec![(1, 42), (1, 43), (1, 44)]
+        ));
+        // last token should be 3
+        assert_eq!(SimpleNFT::last_token(), 3);
+        // get the token
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: None
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(2);
+        assert_eq!(
+            token,
+            Token {
+                id: 2,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 43,
+                parents: Vec::new(),
+                children: None
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(3);
+        assert_eq!(
+            token,
+            Token {
+                id: 3,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 44,
+                parents: Vec::new(),
+                children: None
+            }
+        );
+    });
 }
 
 #[test]
 fn it_works_for_destroying_single_token() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)] ).unwrap();
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new() ));
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 1);
-		// get the old token
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: Some(Vec::new())
-		});
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)]).unwrap();
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new()));
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 1);
+        // get the old token
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: Some(Vec::new())
+            }
+        );
+    });
 }
 
 #[test]
 fn it_works_for_destroying_many_tokens() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43), (1, 44)] ).unwrap();
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1, 2, 3], Vec::new() ));
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 3);
-		// get the old token
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: Some(Vec::new())
-		});
-		let token = SimpleNFT::tokens_by_id(2);
-		assert_eq!(token, Token {
-			id: 2,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 43,
-			parents: Vec::new(),
-			children: Some(Vec::new())
-		});
-		let token = SimpleNFT::tokens_by_id(3);
-		assert_eq!(token, Token {
-			id: 3,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 44,
-			parents: Vec::new(),
-			children: Some(Vec::new())
-		});
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43), (1, 44)]).unwrap();
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1, 2, 3], Vec::new()));
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 3);
+        // get the old token
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: Some(Vec::new())
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(2);
+        assert_eq!(
+            token,
+            Token {
+                id: 2,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 43,
+                parents: Vec::new(),
+                children: Some(Vec::new())
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(3);
+        assert_eq!(
+            token,
+            Token {
+                id: 3,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 44,
+                parents: Vec::new(),
+                children: Some(Vec::new())
+            }
+        );
+    });
 }
 
 #[test]
 fn it_works_for_creating_and_destroy_single_tokens() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)] ).unwrap();
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1], vec![(2, 43)] ));
-		// assert 1 more token was created
-		assert_eq!(SimpleNFT::last_token(), 2);
-		// get the old token
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: Some(vec![2])
-		});
-		let token = SimpleNFT::tokens_by_id(2);
-		assert_eq!(token, Token {
-			id: 2,
-			owner: 2,
-			creator: 1,
-			block_number: 0,
-			metadata: 43,
-			parents: vec![1],
-			children: None
-		});
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)]).unwrap();
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1], vec![(2, 43)]));
+        // assert 1 more token was created
+        assert_eq!(SimpleNFT::last_token(), 2);
+        // get the old token
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: Some(vec![2])
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(2);
+        assert_eq!(
+            token,
+            Token {
+                id: 2,
+                owner: 2,
+                creator: 1,
+                block_number: 0,
+                metadata: 43,
+                parents: vec![1],
+                children: None
+            }
+        );
+    });
 }
 
 #[test]
 fn it_works_for_creating_and_destroy_many_tokens() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43)] ).unwrap();
-		// create a token with no parents
-		assert_ok!(SimpleNFT::run_process(Origin::signed(1), vec![1, 2], vec![(1, 44), (2, 45)] ));
-		// assert 2 more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 4);
-		// get the old tokens
-		let token = SimpleNFT::tokens_by_id(1);
-		assert_eq!(token, Token {
-			id: 1,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 42,
-			parents: Vec::new(),
-			children: Some(vec![3, 4])
-		});
-		let token = SimpleNFT::tokens_by_id(2);
-		assert_eq!(token, Token {
-			id: 2,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 43,
-			parents: Vec::new(),
-			children: Some(vec![3, 4])
-		});
-		// get the new tokens
-		let token = SimpleNFT::tokens_by_id(3);
-		assert_eq!(token, Token {
-			id: 3,
-			owner: 1,
-			creator: 1,
-			block_number: 0,
-			metadata: 44,
-			parents: vec![1,2],
-			children: None
-		});
-		let token = SimpleNFT::tokens_by_id(4);
-		assert_eq!(token, Token {
-			id: 4,
-			owner: 2,
-			creator: 1,
-			block_number: 0,
-			metadata: 45,
-			parents: vec![1,2],
-			children: None
-		});
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43)]).unwrap();
+        // create a token with no parents
+        assert_ok!(SimpleNFT::run_process(
+            Origin::signed(1),
+            vec![1, 2],
+            vec![(1, 44), (2, 45)]
+        ));
+        // assert 2 more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 4);
+        // get the old tokens
+        let token = SimpleNFT::tokens_by_id(1);
+        assert_eq!(
+            token,
+            Token {
+                id: 1,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 42,
+                parents: Vec::new(),
+                children: Some(vec![3, 4])
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(2);
+        assert_eq!(
+            token,
+            Token {
+                id: 2,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 43,
+                parents: Vec::new(),
+                children: Some(vec![3, 4])
+            }
+        );
+        // get the new tokens
+        let token = SimpleNFT::tokens_by_id(3);
+        assert_eq!(
+            token,
+            Token {
+                id: 3,
+                owner: 1,
+                creator: 1,
+                block_number: 0,
+                metadata: 44,
+                parents: vec![1, 2],
+                children: None
+            }
+        );
+        let token = SimpleNFT::tokens_by_id(4);
+        assert_eq!(
+            token,
+            Token {
+                id: 4,
+                owner: 2,
+                creator: 1,
+                block_number: 0,
+                metadata: 45,
+                parents: vec![1, 2],
+                children: None
+            }
+        );
+    });
 }
 
 #[test]
 fn it_fails_for_destroying_single_token_as_other_signer() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)] ).unwrap();
-		// get old token
-		let token = SimpleNFT::tokens_by_id(1);
-		// Try to destroy token as incorrect user
-		assert_err!(SimpleNFT::run_process(Origin::signed(2), vec![1], Vec::new() ), Error::<Test>::NotOwned);
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 1);
-		// asset old token hasn't changed
-		assert_eq!(token, SimpleNFT::tokens_by_id(1));
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)]).unwrap();
+        // get old token
+        let token = SimpleNFT::tokens_by_id(1);
+        // Try to destroy token as incorrect user
+        assert_err!(
+            SimpleNFT::run_process(Origin::signed(2), vec![1], Vec::new()),
+            Error::<Test>::NotOwned
+        );
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 1);
+        // asset old token hasn't changed
+        assert_eq!(token, SimpleNFT::tokens_by_id(1));
+    });
 }
 
 #[test]
 fn it_fails_for_destroying_multiple_tokens_as_other_signer() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(2), Vec::new(), vec![(1, 42)] ).unwrap();
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 43)] ).unwrap();
-		// get old token
-		let token_1 = SimpleNFT::tokens_by_id(1);
-		let token_2 = SimpleNFT::tokens_by_id(2);
-		// Try to destroy token as incorrect user
-		assert_err!(SimpleNFT::run_process(Origin::signed(2), vec![1, 2], Vec::new() ), Error::<Test>::NotOwned);
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 2);
-		// asset old token hasn't changed
-		assert_eq!(token_1, SimpleNFT::tokens_by_id(1));
-		// asset old token hasn't changed
-		assert_eq!(token_2, SimpleNFT::tokens_by_id(2));
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(2), Vec::new(), vec![(1, 42)]).unwrap();
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 43)]).unwrap();
+        // get old token
+        let token_1 = SimpleNFT::tokens_by_id(1);
+        let token_2 = SimpleNFT::tokens_by_id(2);
+        // Try to destroy token as incorrect user
+        assert_err!(
+            SimpleNFT::run_process(Origin::signed(2), vec![1, 2], Vec::new()),
+            Error::<Test>::NotOwned
+        );
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 2);
+        // asset old token hasn't changed
+        assert_eq!(token_1, SimpleNFT::tokens_by_id(1));
+        // asset old token hasn't changed
+        assert_eq!(token_2, SimpleNFT::tokens_by_id(2));
+    });
 }
-
 
 #[test]
 fn it_fails_for_destroying_single_burnt_token() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)] ).unwrap();
-		SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new() ).unwrap();
-		// get old token
-		let token = SimpleNFT::tokens_by_id(1);
-		// Try to destroy token as incorrect user
-		assert_err!(SimpleNFT::run_process(Origin::signed(1), vec![1], vec![(1, 43)] ), Error::<Test>::AlreadyBurnt);
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 1);
-		// asset old token hasn't changed
-		assert_eq!(token, SimpleNFT::tokens_by_id(1));
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42)]).unwrap();
+        SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new()).unwrap();
+        // get old token
+        let token = SimpleNFT::tokens_by_id(1);
+        // Try to destroy token as incorrect user
+        assert_err!(
+            SimpleNFT::run_process(Origin::signed(1), vec![1], vec![(1, 43)]),
+            Error::<Test>::AlreadyBurnt
+        );
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 1);
+        // asset old token hasn't changed
+        assert_eq!(token, SimpleNFT::tokens_by_id(1));
+    });
 }
 
 #[test]
 fn it_fails_for_destroying_multiple_tokens_with_burnt_token() {
-	new_test_ext().execute_with(|| {
-		SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43)] ).unwrap();
-		SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new() ).unwrap();
-		// get old token
-		let token_1 = SimpleNFT::tokens_by_id(1);
-		// get old token
-		let token_2 = SimpleNFT::tokens_by_id(2);
-		// Try to destroy token as incorrect user
-		assert_err!(SimpleNFT::run_process(Origin::signed(1), vec![1, 2], vec![(1, 44)] ), Error::<Test>::AlreadyBurnt);
-		// assert no more tokens were created
-		assert_eq!(SimpleNFT::last_token(), 2);
-		// asset old token hasn't changed
-		assert_eq!(token_1, SimpleNFT::tokens_by_id(1));
-		// asset old token hasn't changed
-		assert_eq!(token_2, SimpleNFT::tokens_by_id(2));
-	});
+    new_test_ext().execute_with(|| {
+        SimpleNFT::run_process(Origin::signed(1), Vec::new(), vec![(1, 42), (1, 43)]).unwrap();
+        SimpleNFT::run_process(Origin::signed(1), vec![1], Vec::new()).unwrap();
+        // get old token
+        let token_1 = SimpleNFT::tokens_by_id(1);
+        // get old token
+        let token_2 = SimpleNFT::tokens_by_id(2);
+        // Try to destroy token as incorrect user
+        assert_err!(
+            SimpleNFT::run_process(Origin::signed(1), vec![1, 2], vec![(1, 44)]),
+            Error::<Test>::AlreadyBurnt
+        );
+        // assert no more tokens were created
+        assert_eq!(SimpleNFT::last_token(), 2);
+        // asset old token hasn't changed
+        assert_eq!(token_1, SimpleNFT::tokens_by_id(1));
+        // asset old token hasn't changed
+        assert_eq!(token_2, SimpleNFT::tokens_by_id(2));
+    });
 }
