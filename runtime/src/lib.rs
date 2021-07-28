2db21a38a04c0bae5fd388ae5facca1159e29f30
Matthew Dean <matthew.dean@digicatapult.org.uk>
2021-07-28 15:40:19 +0100

    Set spec version to match runtime crate version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -96,7 +96,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vitalam-node"),
     impl_name: create_runtime_str!("vitalam-node"),
     authoring_version: 1,
-    spec_version: 101,
+    spec_version: 200,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
