1c73800d115a3fce96ecf67d231fcde677ce8b39
Matthew Dean <matthew.dean@digicatapult.org.uk>
2021-11-10 09:28:53 +0000

    Bump rust toolchain to nightly-2021-11-09 to remove rebuild errors

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,9 @@ set -e
 echo "*** Initializing WASM build environment ***"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup install nightly-2021-03-25
+   rustup install nightly-2021-11-09
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-25
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-09
 # cargo build --release
