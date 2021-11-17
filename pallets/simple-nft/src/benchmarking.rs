08a8833b4729daccf3b805f6ddc38cadba5c0a4f
Jonathan Gray <jonmattgray@gmail.com>
2021-11-17 11:38:31 +0000

    benchmarking and weights

--- pallets/simple-nft/src/benchmarking.rs
+++ pallets/simple-nft/src/benchmarking.rs
@@ -13,9 +13,11 @@ const SEED: u32 = 0;
 
 fn add_nfts<T: Config>(r: u32) -> Result<(), &'static str> {
     let owner: T::AccountId = account("owner", 0, SEED);
+    let mut metadata = BTreeMap::new();
+    metadata.insert(T::TokenMetadataKey::default(), T::TokenMetadataValue::default());
     // let _ = T::Currency::make_free_balance_be(&owner, BalanceOf::<T>::max_value());
 
-    let outputs: Vec<_> = (0..r).map(|_| (owner.clone(), T::TokenMetadata::default())).collect();
+    let outputs: Vec<_> = (0..r).map(|_| (owner.clone(), metadata.clone())).collect();
     SimpleNFT::<T>::run_process(RawOrigin::Signed(owner.clone()).into(), Vec::new(), outputs)?;
 
     let expected_last_token = nth_token_id::<T>(r)?;
@@ -34,10 +36,12 @@ fn mk_inputs<T: Config>(i: u32) -> Result<Vec<T::TokenId>, &'static str> {
     Ok(inputs)
 }
 
-fn mk_outputs<T: Config>(o: u32) -> Result<Vec<(T::AccountId, T::TokenMetadata)>, &'static str> {
+fn mk_outputs<T: Config>(o: u32) -> Result<Vec<(T::AccountId, BTreeMap<T::TokenMetadataKey, T::TokenMetadataValue>)>, &'static str> {
     let owner: T::AccountId = account("owner", 0, SEED);
+    let mut metadata = BTreeMap::new();
+    metadata.insert(T::TokenMetadataKey::default(), T::TokenMetadataValue::default());
     let outputs = (0..o)
-        .map(|_| (owner.clone(), T::TokenMetadata::default()))
+        .map(|_| (owner.clone(), metadata.clone()))
         .collect::<Vec<_>>();
 
     Ok(outputs)
