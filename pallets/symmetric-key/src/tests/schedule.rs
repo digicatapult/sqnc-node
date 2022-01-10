e8a7ff470a91a5637a889ef5f75c6ebcef929a6b
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-01-10 09:28:09 +0000

    Add in event assertions

--- pallets/symmetric-key/src/tests/schedule.rs
+++ pallets/symmetric-key/src/tests/schedule.rs
@@ -23,6 +23,10 @@ fn schedule_after_schedule_period() {
         SymmetricKey::on_initialize(System::block_number());
 
         let init_key = (0..32).collect::<Vec<u8>>();
+        let new_key = vec![
+            83, 89, 77, 77, 69, 84, 82, 73, 67, 95, 75, 69, 89, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0
+        ];
         SymmetricKey::update_key(Origin::root(), init_key.clone()).unwrap();
 
         for _bn in 1..5 {
@@ -30,12 +34,10 @@ fn schedule_after_schedule_period() {
             Scheduler::on_initialize(System::block_number());
         }
 
+        assert_eq!(SymmetricKey::key(), new_key);
         assert_eq!(
-            SymmetricKey::key(),
-            vec![
-                83, 89, 77, 77, 69, 84, 82, 73, 67, 95, 75, 69, 89, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0
-            ]
-        );
+            System::events().iter().rev().nth(1).unwrap().event,
+            Event::pallet_symmetric_key(pallet_symmetric_key::Event::UpdateKey(new_key)),
+        )
     });
 }
