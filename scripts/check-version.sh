118ad07ef7212dd37b5380044ec1eb3f3bc19ddd
Daniel Cooper <danielcooperxyz@gmail.com>
2022-01-17 14:10:08 +0000

    Use yq and log github_ref for branch name in workflow

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -24,16 +24,17 @@ function check_version_greater () {
 }
 
 function get_current_version() {
-  CURRENT_VERSION=$(tomlq package.version -f ./node/Cargo.toml | sed 's/"//g')
+  CURRENT_VERSION=$(tomlq .package.version -f ./node/Cargo.toml | sed 's/"//g')
 
   branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
   branch_name="(unnamed branch)"     # detached HEAD (or possibly github workflow?)
   branch_name=${branch_name##refs/heads/}
 
   if [ $GITHUB_REF != "" ]; then
+    printf "Github reference: %s" $GITHUB_REF
     branch_name=${GITHUB_REF##*/}
   fi
-  
+
   if [ "$branch_name" != "main" ]; then
     CURRENT_VERSION=$(printf '%s-%s' "$CURRENT_VERSION" "$branch_name")
     IS_PRERELEASE=true;
