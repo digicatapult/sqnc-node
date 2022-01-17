206a14cf39ef436662cb68027cc5d2766ab7ba43
Daniel Cooper <danielcooperxyz@gmail.com>
2022-01-17 14:06:00 +0000

    Use github_ref for branch name in workflow

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -27,10 +27,13 @@ function get_current_version() {
   CURRENT_VERSION=$(tomlq package.version -f ./node/Cargo.toml | sed 's/"//g')
 
   branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
-  branch_name="(unnamed branch)"     # detached HEAD
-
+  branch_name="(unnamed branch)"     # detached HEAD (or possibly github workflow?)
   branch_name=${branch_name##refs/heads/}
 
+  if [ $GITHUB_REF != "" ]; then
+    branch_name=${GITHUB_REF##*/}
+  fi
+  
   if [ "$branch_name" != "main" ]; then
     CURRENT_VERSION=$(printf '%s-%s' "$CURRENT_VERSION" "$branch_name")
     IS_PRERELEASE=true;
