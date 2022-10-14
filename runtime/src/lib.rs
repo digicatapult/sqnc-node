8b784969c17841d3ed77300daf18f5cf31fc408b
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-10-14 11:21:41 +0100

    Runtime spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dscp"),
     impl_name: create_runtime_str!("dscp"),
     authoring_version: 1,
-    spec_version: 432,
+    spec_version: 430,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
