c19b7d92edb098b0a0dccc1202a922dbcf452704
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2022-10-26 12:52:37 +0100

    Some progress

--- pallets/process-validation/src/benchmarking.rs
+++ pallets/process-validation/src/benchmarking.rs
@@ -1,7 +1,6 @@
 // ! Benchmarking setup for pallet-template
 
 use super::*;
-
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
 use frame_support::bounded_vec;
 use frame_system::RawOrigin;
@@ -11,15 +10,26 @@ use crate::Pallet as ProcessValidation;
 
 // TODO implement benchmarking
 benchmarks! {
-  create_process {
-  }: _(RawOrigin::Root, T::ProcessIdentifier::default(), bounded_vec![])
-  verify {
-  }
+  // create_process {
+  // }: _(RawOrigin::Root, T::ProcessIdentifier::default(), bounded_vec![])
+  // verify {
+  // }
 
   disable_process {
+      ProcessValidation::<T>::create_process(
+            RawOrigin::Root.into(),
+            T::ProcessIdentifier::default(),
+            bounded_vec![BooleanExpressionSymbol::Restriction(Restriction::None)],
+        );
   }: _(RawOrigin::Root, T::ProcessIdentifier::default(), One::one())
   verify {
+    // assert_eq!(
+      // ProcessModel::<T>::get(T::ProcessIdentifier::default(), One::one()),
+      // Process {
+      //     status: ProcessStatus::Disabled,
+      //     program: bounded_vec![BooleanExpressionSymbol::Restriction(Restriction::None)]
+      // }
+  //)
   }
 }
-
 impl_benchmark_test_suite!(ProcessValidation, crate::mock::new_test_ext(), crate::mock::Test,);
