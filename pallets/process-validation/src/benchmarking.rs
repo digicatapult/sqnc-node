7566dc0710a29bae242c3e00ce8127ccd77aec87
Jonathan Gray <jonmattgray@gmail.com>
2022-11-02 15:46:22 +0000

    disable process + updated weights

--- pallets/process-validation/src/benchmarking.rs
+++ pallets/process-validation/src/benchmarking.rs
@@ -9,6 +9,7 @@ use crate::Pallet as ProcessValidation;
 
 benchmarks! {
   create_process {
+    // valid programs have x Restrictions and (x-1) Ops, therefore number of BooleanExpressionSymbol to add is always odd
     let r in 1 .. (1 + T::MaxProcessProgramLength::get() / 2);
 
     let mut program = BoundedVec::<_, _>::with_bounded_capacity(T::MaxProcessProgramLength::get() as usize);
@@ -19,9 +20,7 @@ benchmarks! {
         program.try_push(BooleanExpressionSymbol::Op(BooleanOperator::And)).unwrap();
     }
 
-  }: _(RawOrigin::Root,
-  T::ProcessIdentifier::default(),
-  program.clone())
+  }: _(RawOrigin::Root, T::ProcessIdentifier::default(), program.clone())
   verify {
     let version = VersionModel::<T>::get(T::ProcessIdentifier::default());
     let process = ProcessModel::<T>::get(T::ProcessIdentifier::default(), version);
@@ -30,19 +29,20 @@ benchmarks! {
   }
 
   disable_process {
-    let mut program = BoundedVec::<_, _>::with_bounded_capacity(1);
+    let mut program = BoundedVec::<_, _>::with_bounded_capacity(T::MaxProcessProgramLength::get() as usize);
     program.try_push(BooleanExpressionSymbol::Restriction(Restriction::None)).unwrap();
 
       ProcessValidation::<T>::create_process(
             RawOrigin::Root.into(),
             T::ProcessIdentifier::default(),
-            program,
-        );
-  }: _(RawOrigin::Root, T::ProcessIdentifier::default(), One::one())
+            program.clone(),
+        ).unwrap();
+  }: _(RawOrigin::Root, T::ProcessIdentifier::default(), 1u32.into())
   verify {
-    // let version = ProcessValidation::<T>::get_version(&T::ProcessIdentifier::default());
-    // let process = ProcessModel::<T>::get(T::ProcessIdentifier::default(), version);
-    // assert_eq!(process.status, ProcessStatus::Disabled)
+    let version = VersionModel::<T>::get(T::ProcessIdentifier::default());
+    let process = ProcessModel::<T>::get(T::ProcessIdentifier::default(), version);
+    assert_eq!(process.status, ProcessStatus::Disabled);
+    assert_eq!(process.program, program);
   }
 }
 impl_benchmark_test_suite!(ProcessValidation, crate::mock::new_test_ext(), crate::mock::Test,);
