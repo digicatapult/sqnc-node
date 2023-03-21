5241827bd342542709740f0ed4ad65bab12fcd43
Matthew Dean <matthew.dean@digicatapult.org.uk>
2023-03-21 14:27:38 +0000

    Fix restriction handedness (#124)
    * Fix restriction handedness
    
    Also adds in additional roles for l3
    
    * Upgrade serdes

--- pallets/process-validation/src/tests/validate_process.rs
+++ pallets/process-validation/src/tests/validate_process.rs
@@ -223,7 +223,7 @@ fn it_fails_when_one_restrictions_fails() {
 }
 
 #[test]
-fn it_succeeds_wth_complex_tree() {
+fn it_succeeds_with_complex_tree() {
     new_test_ext().execute_with(|| {
         ProcessModel::<Test>::insert(
             ProcessIdentifier::A,
@@ -268,7 +268,7 @@ fn it_succeeds_wth_complex_tree() {
 }
 
 #[test]
-fn it_fails_wth_complex_tree() {
+fn it_fails_with_complex_tree() {
     new_test_ext().execute_with(|| {
         ProcessModel::<Test>::insert(
             ProcessIdentifier::A,
@@ -307,6 +307,166 @@ fn it_fails_wth_complex_tree() {
             ValidationResult::<u32> {
                 success: false,
                 executed_len: 5u32
+            }
+        );
+    });
+}
+
+#[test]
+fn it_succeeds_with_handed_expressions_r() {
+    new_test_ext().execute_with(|| {
+        ProcessModel::<Test>::insert(
+            ProcessIdentifier::A,
+            1u32,
+            Process {
+                status: ProcessStatus::Enabled,
+                program: bounded_vec![
+                    BooleanExpressionSymbol::Restriction(Restriction::None),
+                    BooleanExpressionSymbol::Restriction(Restriction::Fail),
+                    BooleanExpressionSymbol::Op(BooleanOperator::NotR),
+                ]
+            }
+        );
+
+        let result = ProcessValidation::validate_process(
+            ProcessFullyQualifiedId {
+                id: ProcessIdentifier::A,
+                version: 1u32
+            },
+            &1u64,
+            &vec![ProcessIO {
+                id: 1u128,
+                roles: BTreeMap::new(),
+                metadata: BTreeMap::new()
+            }],
+            &bounded_vec![]
+        );
+
+        assert_eq!(
+            result,
+            ValidationResult::<u32> {
+                success: true,
+                executed_len: 3u32
+            }
+        );
+    });
+}
+
+#[test]
+fn it_succeeds_with_handed_expressions_l() {
+    new_test_ext().execute_with(|| {
+        ProcessModel::<Test>::insert(
+            ProcessIdentifier::A,
+            1u32,
+            Process {
+                status: ProcessStatus::Enabled,
+                program: bounded_vec![
+                    BooleanExpressionSymbol::Restriction(Restriction::Fail),
+                    BooleanExpressionSymbol::Restriction(Restriction::None),
+                    BooleanExpressionSymbol::Op(BooleanOperator::NotL),
+                ]
+            }
+        );
+
+        let result = ProcessValidation::validate_process(
+            ProcessFullyQualifiedId {
+                id: ProcessIdentifier::A,
+                version: 1u32
+            },
+            &1u64,
+            &vec![ProcessIO {
+                id: 1u128,
+                roles: BTreeMap::new(),
+                metadata: BTreeMap::new()
+            }],
+            &bounded_vec![]
+        );
+
+        assert_eq!(
+            result,
+            ValidationResult::<u32> {
+                success: true,
+                executed_len: 3u32
+            }
+        );
+    });
+}
+
+#[test]
+fn it_fails_with_handed_expressions_r() {
+    new_test_ext().execute_with(|| {
+        ProcessModel::<Test>::insert(
+            ProcessIdentifier::A,
+            1u32,
+            Process {
+                status: ProcessStatus::Enabled,
+                program: bounded_vec![
+                    BooleanExpressionSymbol::Restriction(Restriction::Fail),
+                    BooleanExpressionSymbol::Restriction(Restriction::None),
+                    BooleanExpressionSymbol::Op(BooleanOperator::NotR),
+                ]
+            }
+        );
+
+        let result = ProcessValidation::validate_process(
+            ProcessFullyQualifiedId {
+                id: ProcessIdentifier::A,
+                version: 1u32
+            },
+            &1u64,
+            &vec![ProcessIO {
+                id: 1u128,
+                roles: BTreeMap::new(),
+                metadata: BTreeMap::new()
+            }],
+            &bounded_vec![]
+        );
+
+        assert_eq!(
+            result,
+            ValidationResult::<u32> {
+                success: false,
+                executed_len: 3u32
+            }
+        );
+    });
+}
+
+#[test]
+fn it_fails_with_handed_expressions_l() {
+    new_test_ext().execute_with(|| {
+        ProcessModel::<Test>::insert(
+            ProcessIdentifier::A,
+            1u32,
+            Process {
+                status: ProcessStatus::Enabled,
+                program: bounded_vec![
+                    BooleanExpressionSymbol::Restriction(Restriction::None),
+                    BooleanExpressionSymbol::Restriction(Restriction::Fail),
+                    BooleanExpressionSymbol::Op(BooleanOperator::NotL),
+                ]
+            }
+        );
+
+        let result = ProcessValidation::validate_process(
+            ProcessFullyQualifiedId {
+                id: ProcessIdentifier::A,
+                version: 1u32
+            },
+            &1u64,
+            &vec![ProcessIO {
+                id: 1u128,
+                roles: BTreeMap::new(),
+                metadata: BTreeMap::new()
+            }],
+            &bounded_vec![]
+        );
+
+        assert_eq!(
+            result,
+            ValidationResult::<u32> {
+                success: false,
+                executed_len: 3u32
             }
         );
     });
