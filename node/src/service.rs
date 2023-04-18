8cb456980129324dbd84a4ff419bd8dd46a58942
Jonathan Gray <jonmattgray@gmail.com>
2023-04-18 16:13:45 +0100

    remove comment

--- node/src/service.rs
+++ node/src/service.rs
@@ -285,32 +285,6 @@ pub fn new_full(mut config: Configuration) -> Result<TaskManager, ServiceError> 
             telemetry: telemetry.as_ref().map(Telemetry::handle)
         };
 
-        // let babe = sc_consensus_babe::start_babe::<BabePair, _, _, _, _, _, _, _, _, _>(BabeParams {
-        //     slot_duration,
-        //     client,
-        //     select_chain,
-        //     block_import,
-        //     proposer_factory,
-        //     create_inherent_data_providers: move |_, ()| async move {
-        //         let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
-
-        //         let slot = sp_consensus_babe::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
-        //             *timestamp,
-        //             slot_duration
-        //         );
-
-        //         Ok((slot, timestamp))
-        //     },
-        //     force_authoring,
-        //     backoff_authoring_blocks,
-        //     keystore: keystore_container.sync_keystore(),
-        //     sync_oracle: network.clone(),
-        //     justification_sync_link: network.clone(),
-        //     block_proposal_slot_portion: SlotProportion::new(2f32 / 3f32),
-        //     max_block_proposal_slot_portion: None,
-        //     telemetry: telemetry.as_ref().map(|x| x.handle()),
-        // })?;
-
         // the BABE authoring task is considered essential, i.e. if it
         // fails we take down the service with it.
         task_manager.spawn_essential_handle().spawn_blocking(
