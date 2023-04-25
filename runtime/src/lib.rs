0b1b57f174e9fcee2970cd87bf33c3f7f05b5a99
Jonathan Gray <jonmattgray@gmail.com>
2023-04-25 13:19:29 +0100

    v bump

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -101,7 +101,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dscp"),
     impl_name: create_runtime_str!("dscp"),
     authoring_version: 1,
-    spec_version: 600,
+    spec_version: 800,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
