d24603094e92552af42fbb33e145a115e5aa27b0
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2025-02-13 11:27:11 +0000

    typo

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -58,7 +58,7 @@ pub fn development_config() -> Result<ChainSpec, String> {
             get_account_id_from_seed::<sr25519::Public>("Charlie"),
             get_account_id_from_seed::<sr25519::Public>("Dave"),
             get_account_id_from_seed::<sr25519::Public>("Eve"),
-            get_account_id_from_seed::<sr25519::Public>("Freddie"),
+            get_account_id_from_seed::<sr25519::Public>("Ferdie"),
         ],
         vec![
             get_account_id_from_seed::<sr25519::Public>("Alice"),
